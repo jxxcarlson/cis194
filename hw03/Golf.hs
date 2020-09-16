d265385a0f2f35a37414f57e0f00328eca1f16f7
James Carlson <jxxcarlson@gmail.com>
2020-09-16 01:41:13 -0400

    Shorten (and thereby obfuscate) localMaxima

--- hw03/Golf.hs
+++ hw03/Golf.hs
@@ -7,3 +7,17 @@ skips items =
 skip :: Int -> [a] -> [a]
 skip k items =
   drop 1 $ map snd $ filter (\(i,_) -> i `mod` k == 0) (zip [0..(length items)] items)
+
+{-
+
+  > localMaxima [1, 2, 4, 2, 3, 5, 4, 1]
+  [4,5]
+
+  > localMaxima [1, 2, 3, 4, 5]
+  []
+
+-}
+localMaxima :: [Integer] -> [Integer]
+localMaxima ns =
+  fmap (\(a_, b_, c_) -> b_) $ filter (\(a, b, c) -> b > a && b > c)
+    (zip3 (head ns : take (length ns - 1) ns) ns (drop 1 ns ++ [last ns]))
