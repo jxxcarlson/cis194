181fe41a171993754962fb474084d2041f682fee
James Carlson <jxxcarlson@gmail.com>
2020-09-26 10:03:07 -0400

    Update Risk. There is still a bug (see lines 157-164)

--- hw12/Risk.hs
+++ hw12/Risk.hs
@@ -10,7 +10,7 @@
 module HW12.Risk where
 
 import Control.Monad.Random
-import Data.List (sort)
+import Data.List (sort)``
 
 ------------------------------------------------------------
 -- Die values
@@ -39,115 +39,175 @@ data Battlefield = Battlefield { attackers :: Army, defenders :: Army } deriving
 
 data BattleOutcome = AttackerWins | DefenderWins deriving (Show, Eq)
 
-threeInts :: Rand StdGen (Int, Int, Int)
-threeInts =
-  getRandom >>= \i1 ->
-  getRandom >>= \i2 ->
-  getRandom >>= \i3 ->
-  return (i1,i2,i3)
 
+-- TEST DATA
+b :: Battlefield
+b = Battlefield { attackers = 5, defenders = 5 }
+
+b':: Battlefield
+b' = Battlefield { attackers = 3, defenders = 3 }
+
+bb :: Battlefield
+bb = Battlefield { attackers = 100, defenders = 100 }
+
+
+-- DICE
 
 -- > evalRandIO $ rollDie 4
 --   [DV {unDV = 6},DV {unDV = 4},DV {unDV = 5},DV {unDV = 2}]
 dieRolls :: Int -> Rand StdGen [DieValue]
 dieRolls n = sequence $ replicate n die
 
+
+-- > evalRandIO $ sortedDieRolls 4
+--   [DV {unDV = 6},DV {unDV = 5},DV {unDV = 4},DV {unDV = 3}]
 sortedDieRolls :: Int -> Rand StdGen [DieValue]
 sortedDieRolls n = (reverse . sort) <$> dieRolls n
 
-battles :: Int -> Int -> Rand StdGen [(DieValue, DieValue)]
-battles attackers defenders = 
-  let 
-    n = min attackers defenders
-  in
+--  PURPOSE: produces a generator for n pairs of sorted die rolls
+--  > evalRandIO $ battles 3 3
+--    [(DV {unDV = 4},DV {unDV = 4}),(DV {unDV = 3},DV {unDV = 2}),(DV {unDV = 2},DV {unDV = 2})]
+genPairs :: Int -> Rand StdGen [(DieValue, DieValue)]
+genPairs n = 
     zip <$> sortedDieRolls n <*> sortedDieRolls n
 
+
+
+
+-- BATTLES
+
+--  Determine the battle outcome for a give pair of die rolls
+-- > battleOutcome (DV {unDV = 4},DV {unDV = 4})
+--   DefenderWins
 battleOutcome :: (DieValue, DieValue)  -> BattleOutcome
 battleOutcome (attackersDie, defendersDie) = 
   if attackersDie > defendersDie 
     then AttackerWins
     else DefenderWins
 
+
+-- Generate n random battle o
 -- > evalRandIO $ battleOutcomes 3 5
 --   [DefenderWins,DefenderWins,DefenderWins]
-battleOutcomes :: Int -> Int -> Rand StdGen [BattleOutcome]
-battleOutcomes attackers defenders = 
-  map battleOutcome <$> battles attackers defenders
+battleOutcomes :: Int -> Rand StdGen [BattleOutcome]
+battleOutcomes n = 
+  map battleOutcome <$> genPairs n
 
 
-incrementBattlefield :: Int -> Int -> Battlefield -> Battlefield
-incrementBattlefield i j b = 
-  Battlefield { attackers = attackers  b + i, defenders = defenders b + j }
-
+--  Decrement the number of attackers and defenders on the battlefield
+--   If  b = Battlefield {attackers = 5, defenders = 6}, then
+-- > decrementBattlefield 1 1 b
+--   Battlefield {attackers = 4, defenders = 5}
 decrementBattlefield :: Int -> Int -> Battlefield -> Battlefield
-decrementBattlefield i j b = 
-  Battlefield { attackers = attackers  b - i, defenders = defenders b - j }
-
-b :: Battlefield
-b = Battlefield { attackers = 5, defenders = 6 }
-
-b' :: Rand StdGen Battlefield
-b' = pure b
-
-ba :: Battlefield
-ba = Battlefield { attackers = 1, defenders = 2 }
-
-bd :: Battlefield
-bd = Battlefield { attackers = 3, defenders = 0 }
+decrementBattlefield di dj b = 
+  Battlefield { attackers = attackers  b - di, defenders = defenders b - dj }
 
 
-
-
--- > evalRandIO $ battle b
---   Battlefield {attackers = 3, defenders = 5}
-battle :: Battlefield -> Rand StdGen Battlefield
-battle battlefield = 
+-- Compute the number of times to roll the dice
+-- given the current battlefield
+numberOfBattles :: Battlefield -> Int
+numberOfBattles battlefield =
   let 
     attackers_ = attackers battlefield
     defenders_ = defenders battlefield
     actualAttackers = max 0 $ min 3 (attackers_ - 1)
     actualDefenders = min 2 defenders_
-    genResults = battleOutcomes actualAttackers actualDefenders
-    successfulAttacks = length <$> filter (\x -> x == AttackerWins) <$> genResults 
-    successfulDefenses = length <$> filter (\x -> x == DefenderWins) <$> genResults 
+  in
+    min actualAttackers actualDefenders
+
+
+-- Generate outcomes for random battles given the current battlefield
+--  > evalRandIO $ genBattleOutcomes b
+--    [AttackerWins,AttackerWins]
+genBattleOutcomes :: Battlefield -> Rand StdGen [BattleOutcome]
+genBattleOutcomes battlefield = 
+    battleOutcomes $ numberOfBattles battlefield
+
+-- Carry out the battle defined by the given outcomes
+-- > evalRandIO $ doBattle b outcomes
+--   Battlefield {attackers = 3, defenders = 5}
+doBattle :: Battlefield -> Rand StdGen [BattleOutcome] -> Rand StdGen Battlefield
+doBattle battlefield outcomes = 
+  let 
+    successfulAttacks = length <$> filter (\x -> x == AttackerWins) <$> outcomes 
+    successfulDefenses = length <$> filter (\x -> x == DefenderWins) <$> outcomes 
   in 
     decrementBattlefield <$> successfulDefenses <*> successfulAttacks <*> pure battlefield
 
+-- Generate a new battlefeield
+-- > evalRandIO $ battle b
+--   Battlefield {attackers = 3, defenders = 5}
+battle :: Battlefield -> Rand StdGen Battlefield
+battle b = doBattle b (genBattleOutcomes b)
+
+
+-- INVASIONS
 
 invade_ :: Rand StdGen Battlefield -> Rand StdGen Battlefield
 invade_ b =
   do
-    attackersExhausted <- (<=) <$> (attackers <$> b) <*> (pure 2)
+    attackersExhausted <- (<) <$> (attackers <$> b) <*> (pure 2)
     defendersDefeated <- (==) <$> (defenders <$> b) <*> (pure 0)
     if attackersExhausted || defendersDefeated
       then b
-      else  invade_ $ b >>= battle
+      else invade_ (b >>= battle)
+
+--- EXERCISE 3 ---
 
 -- > evalRandIO $ invade b
---   Battlefield {attackers = 1, defenders = 5}
+--   Battlefield {attackers = 1, defenders = 4}
+--
+-- BUT THE BELOW IS WRONG!
+-- > evalRandIO $ invade b
+-- Battlefield {attackers = 2, defenders = 3}
 invade :: Battlefield -> Rand StdGen Battlefield
 invade b = invade_ $ pure b
 
+--- EXERCISE 4 ---
+
+invadeN :: Int -> Battlefield -> Rand StdGen [Battlefield]
+invadeN n b = replicate n <$> invade b
+
+
+successfulAttacks :: Int -> Battlefield -> Rand StdGen Int
+successfulAttacks n b = length <$> filter attackerWon <$> invadeN n b
+
+
+attackerWon :: Battlefield -> Bool 
+attackerWon b = defenders b == 0
 
 
 
--- NOTES:
--- evalRandIO $ threeInts :: IO (Int, Int, Int)
---
--- > evalRandIO $ threeInts :: IO (Int, Int, Int)
---   (-1855278204539649463,-3128923297145675342,6881384312351252373)
---
--- > :t print
--- print :: Show a => a -> IO ()
---
--- :t (>>=)
--- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+
+-- main b = 
+--   do
+--     putStrLn "\nBattlefield:"
+--     print b
+--     out <- evalRandIO $ genBattleOutcomes b
+--     putStrLn "\nBattle outcomes:"
+--     print out
+--     putStrLn ""
+--     evalRandIO $ invade b 
+--     putStrLn ""
 
 
+main b = 
+  let 
+    outcomes = genBattleOutcomes b
+    finalBattlefield = doBattle b outcomes
+  in
+  do
+    putStrLn "\nBattlefield before:"
+    print b
+    out <- evalRandIO $ outcomes
+    putStrLn "\nBattle outcomes:"
+    print out
+    putStrLn ""
+    putStrLn "\nBattlefield after"
+    fb <- evalRandIO $ finalBattlefield
+    print fb
+    putStrLn "" 
 
-main = do
-    rolls <- evalRandIO $ threeInts
-    print rolls  
 
 
 --- HELPERS ---
